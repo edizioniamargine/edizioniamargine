5be925cc62d36d7e210465dcd872d9e084deb9bf
edizioniamargine <66847207+edizioniamargine@users.noreply.github.com>
2020-06-14 13:33:18 +0200

    tante cose, troppe da ricordare

--- js/randomcover.js
+++ js/randomcover.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
 
   var random = Math.floor(Math.random() * 15) + 1;
 
-  $("#column2").css("background-image", "url(../resources/media/covers/cover" + random + ".png)");
+  $("#column2").css("background-image", "url(../resources/media/covers/cover" + random + ".webp)");
 
 });
